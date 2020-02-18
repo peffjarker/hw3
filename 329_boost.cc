681c620746c279b0098eff2c64e92b37461c50ab
Jeffery Parker <36011518+peffjarker@users.noreply.github.com>
2020-02-18 15:39:49 -0500

    Update 329_boost.cc

--- 329_boost.cc
+++ 329_boost.cc
@@ -105,18 +105,21 @@ int main() {
       total_sum += sum_prob;
       // cout << sum_prob << endl;
     }
+
+#pragma omp critical
+    {
+      cpp_int num;
+      cpp_int den;
+      cpp_int gcd_n_d;
+      long long d = 235092492288000ll;
+      num = sum_prob;
+      den = d;
+      gcd_n_d = gcd(num, den);
+
+      cout << "Probability = " << sum_prob << "/" << 235092492288000ll << endl;
+      // cout << "GCD  = " <<gcd_n_d << endl;
+      cout << "Reduced form:" << endl;
+      cout << sum_prob / gcd_n_d << "/" << d / gcd_n_d << endl;
+    }
   }
-
-  cpp_int num;
-  cpp_int den;
-  cpp_int gcd_n_d;
-  long long d = 235092492288000ll;
-  num = sum_prob;
-  den = d;
-  gcd_n_d = gcd(num, den);
-
-  cout << "Probability = " << sum_prob << "/" << 235092492288000ll << endl;
-  // cout << "GCD  = " <<gcd_n_d << endl;
-  cout << "Reduced form:" << endl;
-  cout << sum_prob / gcd_n_d << "/" << d / gcd_n_d << endl;
 }
